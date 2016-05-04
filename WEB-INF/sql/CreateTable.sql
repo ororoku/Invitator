5f09e0333a05e16115b9ba71c148b6e784008bc8
oroku <oroku@r.recruit.co.jp>
2016-05-04 11:47:58 +0900

    sql更新

--- WEB-INF/sql/CreateTable.sql
+++ WEB-INF/sql/CreateTable.sql
@@ -14,7 +14,8 @@ create table usertable(
 	description nvarchar2(40),
 	address nvarchar2(40),
 	is_delete number(1),
-	constraint pk_user primary key( user_id )
+	constraint pk_user primary key( user_id ),
+	constraint ck_userflag CHECK (is_delete IN ('1', '0'))
 );
 
 create table grouptable(
@@ -22,7 +23,8 @@ create table grouptable(
 	group_image nvarchar2(40),
 	description nvarchar2(40),
 	is_delete number(1),
-	constraint pk_group primary key( group_name )
+	constraint pk_group primary key( group_name ),
+	constraint ck_groupflag CHECK (is_delete IN ('1', '0'))
 );
 
 create table post(
@@ -34,7 +36,8 @@ create table post(
 	time_end number(4),
 	is_delete number(1),
 	create_time DATE DEFAULT SYSDATE,
-	constraint pk_post primary key( post_id )
+	constraint pk_post primary key( post_id ),
+	constraint ck_postflag CHECK (is_delete IN ('1', '0'))
 );
 
 create table matching(
@@ -42,7 +45,8 @@ create table matching(
 	go_time number(4),
 	purpose number(1),
 	is_closed number(1),
-	constraint pk_matching primary key( matching_id )
+	constraint pk_matching primary key( matching_id ),
+	constraint ck_closedflag CHECK (is_closed IN ('1', '0'))
 );
 
 create table usergroup(
@@ -50,6 +54,7 @@ create table usergroup(
 	user_id nchar(20),
 	group_name nvarchar2(20),
 	is_ignore number(1),
-	constraint pk_usergroup primary key( user_group_id )
+	constraint pk_usergroup primary key( user_group_id ),
+	constraint ck_ignoreflag CHECK (is_ignore IN ('1', '0'))
 );
 
