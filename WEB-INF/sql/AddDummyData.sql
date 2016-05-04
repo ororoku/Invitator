d445510911da1f56fe31357ffc8afab48920358d
oroku <oroku@r.recruit.co.jp>
2016-05-04 16:34:57 +0900

    AddDummyData.sqlを修正

--- WEB-INF/sql/AddDummyData.sql
+++ WEB-INF/sql/AddDummyData.sql
@@ -8,9 +8,9 @@ insert into grouptable values ('JAVAJAVA‡h','url','–‹’£‚Å‡h‚ğ‚µ‚Ä‚¢‚Ü‚·',0);
 insert into grouptable values ('JAVAJAVA‡h2','url','–‹’£2‚Å‡h‚ğ‚µ‚Ä‚¢‚Ü‚·',0);
 insert into grouptable values ('JAVAJAVA‡h3','url','–‹’£3‚Å‡h‚ğ‚µ‚Ä‚¢‚Ü‚·',1);
 
-insert into post values ('00001','aaaa','00001',1,'1800','2000',0,);
-insert into post values ('00002','1111','00002',1,'1800','2000',0,);
-insert into post values ('00003','rrrr','00002',1,'1900','2100',0,);
+insert into post values ('00001','aaaa','00001',1,'1800','2000',0,'');
+insert into post values ('00002','1111','00002',1,'1800','2000',0,'');
+insert into post values ('00003','rrrr','00002',1,'1900','2100',0,'');
 
 insert into matching values ('00001','1900',1,0);
 insert into matching values ('00002','1930',0,1);
